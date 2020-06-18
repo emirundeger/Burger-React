ee3f14b07280cf636a9303dc44ce0a4ea91fc57f
emirundeger <emirundeger@gmail.com>
2020-06-18 21:21:59 +0100

    Show order summary once the order now button clicked

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4, //base price
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   };
 
   updatePurchasableState(ingredients) {
@@ -67,6 +68,10 @@ class BurgerBuilder extends Component {
     this.updatePurchasableState(updatedIngredients);
   };
 
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -76,7 +81,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -85,6 +90,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice}
         />
       </Aux>
