8cf7433fd5681e5a59171029c09a17a7c6a7913f
emirundeger <emirundeger@gmail.com>
2020-08-05 13:31:27 +0100

    Adding order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -114,16 +114,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false });
-        this.props.history.push("/");
-      })
-      .catch(response => {
-        this.setState({ loading: false });
-      });
   };
   checkValidity(value, rules) {
     let isValid = true;
