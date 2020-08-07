50d470710c0985fe105b36f77c8c677f530dfa5c
emirundeger <emirundeger@gmail.com>
2020-08-07 15:08:33 +0100

    Adding actions for Auth form

---
+++ src/store/actions/auth.js
@@ -0,0 +1,47 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+
+//To set a loading state and show a spinner
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
+
+export const auth = (email, password) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAT_gEUI2Bq0DX6DamIHnSVfB-FlOPDb-Y",
+        authData
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
