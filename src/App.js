5cd790189e0f19030c81ae162839e498061d4e32
emirundeger <emirundeger@gmail.com>
2020-08-09 14:15:06 +0100

    We want the user is in login state with localStorage if he reloads the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Route path="/" exact component={BurgerBuilder} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/auth" component={Auth} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/orders" component={Orders} />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Route path="/" exact component={BurgerBuilder} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/orders" component={Orders} />
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
