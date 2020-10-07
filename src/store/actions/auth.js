7d510ed551ccd2edd601c5a122c03d8cfbbfc183
Emir Undeger <e.undeger@aybola.com>
2020-10-07 13:00:43 +0100

    Handling Auto-Sign-In with a Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,4 @@
 import * as actionTypes from "./actionTypes";
-import axios from "axios";
 
 //To set a loading state and show a spinner
 export const authStart = () => {
@@ -24,9 +23,6 @@ export const authFail = error => {
 };
 
 export const logout = () => {
-  // localStorage.removeItem("token");
-  // localStorage.removeItem("expirationDate");
-  // localStorage.removeItem("userId");
   return {
     type: actionTypes.AUTH_INITIATE_LOGOUT
   };
@@ -62,23 +58,7 @@ export const setAuthRedirectPath = path => {
 };
 
 export const authCheckState = () => {
-  return dispatch => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      //dispatch(logout());
-    } else {
-      const expirationDate = new Date(localStorage.getItem("expirationDate"));
-      if (expirationDate < new Date()) {
-        //dispatch(logout());
-      } else {
-        const userId = localStorage.getItem("userId");
-        dispatch(authSuccess(token, userId));
-        dispatch(
-          checkAuthTimeout(
-            (expirationDate.getTime() - new Date().getTime()) / 1000
-          )
-        );
-      }
-    }
+  return {
+    type: actionTypes.AUTH_CHECK_STATE
   };
 };
